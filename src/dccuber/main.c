8f7332514649c852d14db1a6e1cb89b1f42e31f1
MAGDALENA VALDES <mvaldes12@uc.cl>
2021-08-30 20:31:45 -0400

    semaforos

--- src/dccuber/main.c
+++ src/dccuber/main.c
@@ -39,47 +39,55 @@ int main(int argc, char const *argv[])
   }
   printf("\n");
   printf("Liberando memoria...\n");
+  char* delay_1[] = {data_in->lines[1][2]};
+  char* delay_2[] = {data_in->lines[1][3]};
+  char* delay_3[] = {data_in->lines[1][4]};
   input_file_destroy(data_in);
   
   int fabrica = fork();
   if (fabrica == 0){
+    //int pid_fabrica = getpid();
+    //printf("I'm fabrica and my pid is int: %i\n", pid_fabrica);
     //crear N repartidores cada x seg
     //conectar señal de cambio de semaforo con un handler
     //wait a todos sus repartidores
-    //exit
+    exit(0);
   }
-  int pid_fabrica = getpid();
-  printf("I'm fabrica and my pid is int: %i\n", pid_fabrica);
-  char pid_fabrica_char = pid_fabrica;
-  printf("I'm fabrica and my pid is char: %i\n", pid_fabrica_char);
+
+  waitpid(fabrica,0,0); //quizás hay que cambiar el NULL
+  
+  //char pid_fabrica_char = pid_fabrica;
+  //printf("I'm fabrica and my pid is char: %i\n", pid_fabrica_char);
 
   //logica main
   // wait fabrica
+  char char_fabrica[5];
+  sprintf(char_fabrica, "%i", fabrica);
 
   int semaforo_1 = fork();
   if (semaforo_1 == 0){
-    execlp("./semaforo", "1", "7", "3", NULL);
+    execlp("./semaforo", "1", *delay_1, char_fabrica, NULL);
     printf("No se ejecutó semaforo 1\n");
     exit(0);
   }
+  
   
   int semaforo_2 = fork();
   if (semaforo_2 == 0){
-    execlp("./semaforo", "2", "8", "3", NULL);
-    printf("No se ejecutó semaforo 1\n");
+    execlp("./semaforo", "2", *delay_2, char_fabrica, NULL);
+    printf("No se ejecutó semaforo 2\n");
     exit(0);
   }
+  
 
   int semaforo_3 = fork(); 
   if (semaforo_3 == 0){
-      execlp("./semaforo", "3", "9", "3", NULL);
-      printf("No se ejecutó semaforo 1\n");
+      execlp("./semaforo", "3", *delay_3, char_fabrica, NULL);
+      printf("No se ejecutó semaforo 3\n");
       exit(0);
     }
   
- 
-
-  wait(NULL); //quizás hay que cambiar el NULL
+  
   printf("Proceso principal ha terminado.\n");
   return 0;
 }
--- src/semaforo/main.c
+++ src/semaforo/main.c
@@ -4,4 +4,7 @@
 int main(int argc, char const *argv[])
 {
   printf("I'm the SEMAFORO process and my PID is: %i\n", getpid());
+  printf("id:%s \n" , argv[0]);
+  printf("delay:%s \n" , argv[1]);
+  printf("parent:%s \n" , argv[2]);
 }
